30220a117030f414abe3515ab6ae80db1db74e3c
Ethan Miller <baseballfreak07@gmail.com>
2020-01-29 13:56:09 -0700

    Added method to calculate distance between two points in Graph class

--- Project1/src/Graph.java
+++ Project1/src/Graph.java
@@ -30,6 +30,7 @@ public class Graph {
         for(int i= 0; i < nodelist.length; i++) {
             float x = (float) Math.random();
             float y = (float) Math.random();
+
             while(x == 0 || y == 0) {
                 x = (float) Math.random();
                 y = (float) Math.random();
@@ -49,6 +50,14 @@ public class Graph {
 
 
         }
+    }
+
+    public float getDistance(float x1, float y1, float x2, float y2)
+    {
+        float dx = x1 - x2;
+        float dy = y1 - y2;
+        float dist = (float) Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+        return dist;
     }
 
 }
